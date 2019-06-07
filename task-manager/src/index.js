ee1b462dbca89bfffb5072724b50b826da43eb24
Matthew Lui <mlui@uniting.org>
2019-06-07 15:23:06 +1000

    updated

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,9 +16,33 @@ app.post('/users', (req, res) => {
     .then(result => {
       res.status(201).send(result);
     })
-    .catch(error => {
+    .catch(e => {
       res.status(400);
-      res.send(error);
+      res.send(e);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
@@ -29,9 +53,33 @@ app.post('/tasks', (req, res) => {
     .then(result => {
       res.status(201).send(result);
     })
-    .catch(error => {
+    .catch(e => {
       res.status(400);
-      res.send(error);
+      res.send(e);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => {
+      res.send(tasks);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then(tasks => {
+      if (!tasks) {
+        return res.status(404).send();
+      }
+      res.send(tasks);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
