a78230a28740d6bd99081a1df56535b76887a8cd
Matthew Lui <mlui@uniting.org>
2019-06-13 15:22:03 +1000

    updated

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,13 +1,27 @@
 require('../src/db/mongoose');
-const User = require('../src/models/User');
+const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5cf98c104c56fe3738d7f726', { age: 1 })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
+// User.findByIdAndUpdate('5cf98c104c56fe3738d7f726', { age: 1 })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('5cf98c104c56fe3738d7f726', 10)
   .then(result => {
-    console.log(result);
+    console.log('count: ' + result);
   })
   .catch(e => {
     console.log(e);
--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,13 +1,27 @@
 require('../src/db/mongoose.js');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5cf7388f2ce4084d982a92f1')
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
+// Task.findByIdAndDelete('5cf7388f2ce4084d982a92f1')
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async id => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount('5d0173c093d44b30b89ea1ff')
   .then(result => {
-    console.log(result);
+    console.log('result: ' + result);
   })
   .catch(e => {
     console.log(e);
