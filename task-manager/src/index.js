adb8949dbd0ab45f31580544afc71fbc4e799ffa
Matthew Lui <mlui@uniting.org>
2019-06-14 15:29:11 +1000

    updated

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,42 +8,37 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(result => {
-      res.status(201).send(result);
-    })
-    .catch(e => {
-      res.status(400);
-      res.send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.post('/tasks', (req, res) => {
