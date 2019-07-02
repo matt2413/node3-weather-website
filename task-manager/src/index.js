c9c016f3972475e67664d7520e4a465b6e0c046c
Matthew Lui <mlui@uniting.org>
2019-07-02 15:28:25 +1000

    updated

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,6 +5,12 @@ const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
