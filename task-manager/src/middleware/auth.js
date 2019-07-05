23a56acd0c07c96092c8267ef63b0e069fac65a7
Matthew Lui <mlui@uniting.org>
2019-07-05 15:24:58 +1000

    updated

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -5,15 +5,10 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
     const decoded = jwt.verify(token, 'thisismynewcourse');
-    const user = await User.findOne({
-      _id: decoded._id,
-      'tokens:token': token
-    });
-
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
     if (!user) {
       throw new Error();
     }
-
     req.user = user;
     next();
   } catch (e) {
